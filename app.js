21c3fcc06d2bba61410ddcb57be186d950d9ab96
Brian Schroer <schroer.brian@gmail.com>
2013-08-13 15:03:02 -0700

    Basic chat functionality working

--- app.js
+++ app.js
@@ -11,4 +11,17 @@ app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
+//define behavior for an individual socket
+
+io.sockets.on('connection', function(socket) {  //connection is a reserved handler
+
+	socket.on("send message", function(data) {
+		//console.log(data);
+		io.sockets.emit('new message', data)
+		// socket.broadcast.emit - sends message to everyone but me; unlike io.sockets.emit
+	});
+
+});
+
+
 server.listen(3000);
